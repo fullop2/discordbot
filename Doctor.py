2f7be31c86964677f77b092a3f892cd2b986bde0
fullop2 <parkth32@gmail.com>
2019-05-05 17:22:34 +0900

    adsf

--- Doctor.py
+++ Doctor.py
@@ -27,13 +27,13 @@ async def on_ready():
 
 
 timeTable =[
-[datetime.time(1,40),datetime.time(10,40),datetime.time(15,40),datetime.time(19,40),datetime.time(23,25)],
- [datetime.time(1,40),datetime.time(10,40),datetime.time(15,40),datetime.time(19,40),datetime.time(23,5),datetime.time(23,25)],
- [datetime.time(1,40),datetime.time(15,40),datetime.time(19,40),datetime.time(22,55),datetime.time(23,25)],
- [datetime.time(1,40),datetime.time(10,40),datetime.time(15,40),datetime.time(19,40),datetime.time(23,25)],
- [datetime.time(1,40),datetime.time(10,40),datetime.time(15,40),datetime.time(19,40),datetime.time(23,25),datetime.time(23,55)],
- [datetime.time(1,40),datetime.time(10,40),datetime.time(15,40),datetime.time(18,40)],
- [datetime.time(1,40),datetime.time(10,40),datetime.time(15,40),datetime.time(16,20),datetime.time(19,40),datetime.time(23,25)]]
+ [datetime.time(2,0),datetime.time(11,0),datetime.time(16,0),datetime.time(20,0),datetime.time(23,45)],
+ [datetime.time(2,0),datetime.time(11,0),datetime.time(16,0),datetime.time(20,0),datetime.time(23,45)],
+ [datetime.time(2,0),datetime.time(16,0),datetime.time(20,0),datetime.time(23,15),datetime.time(23,45)],
+ [datetime.time(2,0),datetime.time(11,0),datetime.time(16,0),datetime.time(20,0),datetime.time(23,45)],
+ [datetime.time(2,0),datetime.time(11,0),datetime.time(16,0),datetime.time(20,0),datetime.time(23,45),datetime.time(00,15)],
+ [datetime.time(2,0),datetime.time(11,0),datetime.time(16,0),datetime.time(19,0)],
+ [datetime.time(00,15),datetime.time(2,0),datetime.time(11,0),datetime.time(16,0),datetime.time(17,0),datetime.time(20,0),datetime.time(23,45)]]
 
 boss =[
  [['크자카'],['크자카','누베르'],['크자카','쿠툼'],['카란다','누베르'],['오핀']],
@@ -42,17 +42,17 @@ boss =[
  [['쿠툼'],['크자카','누베르'],['카란다','쿠툼',],['누베르','쿠툼'],['가모스']],
  [['카란다'],['크자카','쿠툼'],['카란다','누베르'],['크자카','쿠툼'],['오핀']],
  [['카란다','누베르'],['누베르','쿠툼'],['카란다','크자카'],['귄트','무라카'],['가모스']],
- [['카란다','쿠툼'],['크자카','누베르'],['카란다','쿠툼'],['벨'],['누베르','쿠툼'],['카란다']]]
+ [['가모스'],['카란다','쿠툼'],['크자카','누베르'],['카란다','쿠툼'],['벨'],['누베르','쿠툼'],['카란다']]]
 
-img = {'누베르' : 'https://imgur.com/fulOkgC',
-       '무라카' : 'https://imgur.com/UuJAybi',
-       '카란다' : 'https://imgur.com/JyT9yPs',
-       '벨'     : 'https://imgur.com/OfOKlkG',
-       '오핀'   : 'https://imgur.com/C5MvgX4',
-       '쿠툼'   : 'D:\Project\Python\discordbot\doctor\쿠툼.png',
-       '크자카' : 'https://imgur.com/FASWhuN',
-       '가모스' : 'https://imgur.com/Oj76uzE',
-       '귄트'   : 'https://imgur.com/r32BVA3'}
+img = {'누베르' : 'https://raw.githubusercontent.com/fullop2/discordbot/master/pic/%EB%88%84%EB%B2%A0%EB%A5%B4.png',
+       '무라카' : 'https://raw.githubusercontent.com/fullop2/discordbot/master/pic/%EB%AC%B4%EB%9D%BC%EC%B9%B4.png',
+       '카란다' : 'https://raw.githubusercontent.com/fullop2/discordbot/master/pic/%EC%B9%B4%EB%9E%80%EB%8B%A4.png',
+       '벨'     : 'https://raw.githubusercontent.com/fullop2/discordbot/master/pic/%EB%B2%A8.png',
+       '오핀'   : 'https://raw.githubusercontent.com/fullop2/discordbot/master/pic/%EC%98%A4%ED%95%80.png',
+       '쿠툼'   : 'https://raw.githubusercontent.com/fullop2/discordbot/master/pic/%EC%BF%A0%ED%88%BC.png',
+       '크자카' : 'https://raw.githubusercontent.com/fullop2/discordbot/master/pic/%ED%81%AC%EC%9E%90%EC%B9%B4.png',
+       '가모스' : 'https://raw.githubusercontent.com/fullop2/discordbot/master/pic/%EA%B0%80%EB%AA%A8%EC%8A%A4.png',
+       '귄트'   : 'https://raw.githubusercontent.com/fullop2/discordbot/master/pic/%EA%B7%84%ED%8A%B8.png'}
 
 def getBossIndex(weekday,now) :
     for t in range(0,len(timeTable[weekday])) :
@@ -74,12 +74,12 @@ async def alram(message):
     bosssec = bosstime.hour * 3600 + bosstime.minute * 60 + bosstime.second
     nowsec = now.time().hour * 3600 + now.time().minute * 60 + now.time().second
 
-    emb = discord.Embed(title=str(bosstime), description=str(bosstime) + '\n' +str(datetime.timedelta(seconds=bosssec - nowsec)), color=0x00ff00)
-    await message.channel.send(embed = emb)  
     for oneboss in bossname :
         emb = discord.Embed(title=str(oneboss),color=0x00ff00)
         emb.set_image(url=img[oneboss])
-        await message.channel.send(embed = emb)  
+        await message.channel.send(embed = emb)
+    emb = discord.Embed(title='보스 젠 시간', description=str(bosstime) + '\n' +str(datetime.timedelta(seconds=bosssec - nowsec)), color=0x00ff00)
+    await message.channel.send(embed = emb)  
     return
 
 flag = False
